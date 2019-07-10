d7a6d3331fb36a8f0624cb796543d39a89e0016b
Dathan Bennett <dathanb@netflix.com>
2019-07-09 19:44:48 -0700

    Add tests for readmark rendering

--- src/readmark/ReadmarkComponent.test.js
+++ src/readmark/ReadmarkComponent.test.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
-import { ReadmarkComponent } from './readmark';
+import {ReadmarkComponent} from './Readmark';
+import {Readmark} from './types';
 
-it('Displays a message when loading', () => {
-    const renderer = new ShallowRenderer();
-    renderer.render(<ReadmarkComponent loading={true} currentUrl={""} />);
-    const result = renderer.getRenderOutput();
+describe('ReadmarkComponent', () => {
+    let renderer;
+    beforeEach(() => {
+        renderer = new ShallowRenderer();
+    });
 
-    expect(result.type).toBe('p');
-    expect(result.props.children).toEqual("Loading...");
+    test('Displays a message when loading', () => {
+        renderer.render(<ReadmarkComponent loading={true} currentUrl={""}/>);
+        const result = renderer.getRenderOutput();
+
+        expect(result.type).toBe('p');
+        expect(result.props.children).toEqual("Loading...");
+    });
+
+    test("Renders the current url as missing when there is no readmark for the current context", () => {
+        let component = <ReadmarkComponent loading={false}
+                                           currentUrl={"http://www.example.com"}
+                                           readmark={null}
+        />;
+        const result = renderer.render(component);
+        expect(result.type).toBe('p');
+        expect(result.props.className).toBe('missing');
+    });
+
+    test("Renders the current url as mismatched when it doesn't match the readmark for the current context", () => {
+        let component = <ReadmarkComponent loading={false}
+                                           currentUrl={"http://www.example.com/1"}
+                                           readmark={new Readmark("http://www.example.com/2")}
+        />;
+        const result = renderer.render(component);
+        expect(result.type).toBe('p');
+        expect(result.props.className).toBe('mismatched');
+    });
+
+    test("Renders the current url as matched when it matches the readmark for the current context", () => {
+        let component = <ReadmarkComponent loading={false}
+                                           currentUrl={"http://www.example.com/1"}
+                                           readmark={new Readmark("http://www.example.com/1")}
+        />;
+        const result = renderer.render(component);
+        expect(result.type).toBe('p');
+        expect(result.props.className).toBe('matched');
+
+    });
 });
